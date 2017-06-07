74756fb16919872552799be0e04a740263b59b80
Madankapoor <madankapoor10@gmail.com>
2017-06-07 21:24:03 +0530

    coded solution1

---
+++ solution1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*   There are n registration numbers in an array, some of them are repeated, 
+  write an algorithm to find the repeated numbers in time complexity of O(nlogn) or O(n) */
+
+int main()
+{
+   int n;
+   cin >> n; // No. of elements
+   
+   unordered_map<int,int> A; // in c++ 11 or 14
+   for(int i=0;i<n;i++)
+   {
+      int d;
+      cin >> d;
+	  A[d]++;
+   }
+   
+   for(auto it:A)
+   if(it.second > 1)
+     cout << it.first << " "; // output
+   
+   return 0;
+}
