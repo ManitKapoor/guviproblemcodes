4510bfda88dd31a5b232edc2708771c00503afac
Madankapoor <madankapoor10@gmail.com>
2017-06-07 22:03:54 +0530

    coded solution2 again

--- solution2.cpp
+++ solution2.cpp
@@ -14,20 +14,21 @@ int main()
    for(int i=0;i<n;i++)
      cin >> A[i];
 	 
-   sort(A.rbegin(),A.rend());	 
+   sort(A,A+n);	 
    
    string ans = "";
    
-   for(int i=0;i<n;i++)
-    ans = ans + to_string(ans);
+   for(int i=n-1;i>=0;i--)
+    ans = ans + to_string(A[i]);
 	
 	string fans = "";
 	
     for(int i=0;i<ans.length();i++)
-      if(i%3 == 0)
+      if(i%3 == 0 && i != ans.length()-1)
         fans = fans + ans[i] + ",";
       else 
         fans = fans + ans[i]; 	  
+   cout << fans;
    
    return 0;
 }
